e485a1a4d4f5b672ad67d66189bcd8cc6b2dbccf
Alex Hoffman <alexhoffman617@gmail.com>
2020-03-19 16:16:11 -0400

    update nomination order

--- src/app/conversions.ts
+++ src/app/conversions.ts
@@ -61,16 +61,16 @@ export function getPositions(positions: number[]): string[] {
 }
 
 export const Owners = [
-  { display: "Adam Fromal", dbValue: "Adam", nom: 0 },
-  { display: "Arjun Baradwaj", dbValue: "Arjun", nom: 1 },
-  { display: "Brendan Prin", dbValue: "Brendan", nom: 2 },
-  { display: "Bretton McIlrath", dbValue: "Bretton", nom: 3 },
-  { display: "Chris Glazier", dbValue: "Chris", nom: 4 },
-  { display: "Eric Fleury", dbValue: "Eric", nom: 5 },
-  { display: "Jacob Newcomer", dbValue: "Jacob", nom: 6 },
-  { display: "Michael Tumey", dbValue: "Michael", nom: 7 },
-  { display: "Robert Ray", dbValue: "Robert", nom: 8 },
-  { display: 'Alex "Shorty" Hoffman', dbValue: "Shorty", nom: 9 }
+  { display: "Adam Fromal", dbValue: "Adam", nom: 9 },
+  { display: "Arjun Baradwaj", dbValue: "Arjun", nom: 7 },
+  { display: "Brendan Prin", dbValue: "Brendan", nom: 3 },
+  { display: "Bretton McIlrath", dbValue: "Bretton", nom: 4 },
+  { display: "Chris Glazier", dbValue: "Chris", nom: 8 },
+  { display: "Eric Fleury", dbValue: "Eric", nom: 1 },
+  { display: "Jacob Newcomer", dbValue: "Jacob", nom: 0 },
+  { display: "Michael Tumey", dbValue: "Michael", nom: 6 },
+  { display: "Robert Ray", dbValue: "Robert", nom: 5 },
+  { display: 'Alex "Shorty" Hoffman', dbValue: "Shorty", nom: 2 }
 ];
 
 export function getDBOwner(displayValue) {
